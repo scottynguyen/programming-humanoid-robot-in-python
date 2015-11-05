f6d26b22b821ce7405979e714b83a989615dc1c9
scottynguyen <scottvietphongnguyen@yahoo.de>
2015-11-05 12:50:47 +0100

    PID ohne Prediction

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 4
+        self.Ki = 3
+        self.Kd = 2
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,6 +53,12 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
+        error = target- sensor
+        self.e1 += error
+        P= self.Kp * error
+        I= self.Ki * self.e1* self.dt
+        D= self.Kd*error/self.dt
+        self.u= P+I+D
 
         return self.u
 
